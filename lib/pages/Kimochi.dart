15b533ba9a4542ef68e5fa5a013e08166ac39452
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-06 20:01:41 +0000

    Initial

--- lib/pages/Kimochi.dart
+++ lib/pages/Kimochi.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class Kimochi extends StatefulWidget {
+  const Kimochi({Key? key}) : super(key: key);
+
+  @override
+  State<Kimochi> createState() => _KimochiState();
+}
+
+class _KimochiState extends State<Kimochi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
