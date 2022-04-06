c59741c00d17292bdbd7e5afa9ec4d444543f323
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-06 19:51:48 +0000

    Initial

--- lib/pages/next.dart
+++ lib/pages/next.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class nextpage extends StatefulWidget {
+  const nextpage({Key? key}) : super(key: key);
+
+  @override
+  State<nextpage> createState() => _nextpageState();
+}
+
+class _nextpageState extends State<nextpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
