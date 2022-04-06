db23a984c44927fc7e98e9dbea1bfcde6d74ccca
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-06 20:02:12 +0000

    Initial

--- lib/pages/Kimochi.dart
+++ lib/pages/Kimochi.dart
@@ -10,6 +10,9 @@ class Kimochi extends StatefulWidget {
 class _KimochiState extends State<Kimochi> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+
+
+    );
   }
 }
