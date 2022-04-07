1071837899bf65e5bf01e7ca379f3dbbbb368845
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-07 22:09:05 +0000

    second state

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,1 +1,11 @@
 import 'package:flutter/material.dart';
+
+
+class tester extends StatelessWidget {
+  const tester({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
