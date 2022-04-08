e69087c3c957528d37596b2be212207991a3ed98
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-08 17:56:40 +0000

    second state

--- lib/pages/next.dart
+++ lib/pages/next.dart
@@ -70,7 +70,7 @@ Card(
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children:<Widget> [
-        Text("What Do "),
+        Text("What Do You"),
       ],
     ),
   ),
