aaa610331d2d258e0f2af76870b74187da69c049
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-12 20:08:02 +0000

    second state

---
+++ lib/pages/load.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class Loadin extends StatefulWidget {
+  const Loadin({Key? key}) : super(key: key);
+
+  @override
+  State<Loadin> createState() => _LoadinState();
+}
+
+class _LoadinState extends State<Loadin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
